72544c7fdad6621c7c908d2283d8d495b68dc7ee
Eric <ericdongju@outlook.com>
2014-07-28 22:47:55 -0700

    Minor optimization fixes.

--- Preloader.js
+++ Preloader.js
@@ -3,6 +3,7 @@ function preloader() {
         var img1 = new Image();
         var img2 = new Image();
         var img3 = new Image();
+        var img4 = new Image();
 
         img1.src = "img/scroll.png";
         img2.src = "img/hscroll.png";
